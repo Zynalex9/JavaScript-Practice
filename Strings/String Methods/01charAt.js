334227817931f0ef42fef37080660d98bc23c0f6
Zynalex9 <alzn952@gmail.com>
2024-07-04 17:03:59 +0500

    check if first and last characters of a string are the same.

--- Strings/String Methods/01charAt.js
+++ Strings/String Methods/01charAt.js
@@ -1,13 +1,23 @@
+// Question 1: Write a function that returns the middle character of a string. If the string length is odd, return the middle character; if even, return the middle two characters.
+const str = "HELLO";
+const str1 = "HELLOH";
+console.log(str.length);
 function middleOf(string) {
-    const middle = Math.floor(string.length / 2); 
-  
-    if (string.length % 2 === 1) {
-      return string.charAt(middle); 
-    } else {
-      return string.slice(middle - 1, middle + 1); 
-    }
+  const middle = Math.floor(string.length / 2);
+
+  if (string.length % 2 === 1) {
+    return string.charAt(middle);
+  } else {
+    return string.slice(middle - 1, middle + 1);
   }
-  
-  const str = "HELLO"
-  console.log(str.length); 
-  console.log(middleOf(str)); 
+}
+console.log(middleOf(str));
+// Question 2: Create a function that checks if the first and last characters of a string are the same.
+ function firstLast(string) {
+  if (string.charAt(0) === string.charAt(string.length - 1)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(firstLast(str1))
