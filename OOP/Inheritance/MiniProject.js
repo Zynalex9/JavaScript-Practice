27b724267d9a6c4022de744c13e67f38d0f86917
Zynalex9 <alzn952@gmail.com>
2024-07-11 21:14:41 +0500

    Creating objects/instances

--- OOP/Inheritance/MiniProject.js
+++ OOP/Inheritance/MiniProject.js
@@ -1,31 +1,38 @@
-/*Base Class: Vehicle – This will be the parent class with common properties and methods.
-Derived Classes: Car and Bike – These will inherit from Vehicle and have additional specific properties and methods.*/
 class Vehicle {
   constructor(make, model, year) {
     this.make = make;
     this.model = model;
     this.year = year;
   }
+
   getVehicleInfo() {
     return `${this.year} ${this.make} ${this.model}`;
   }
 }
-class car extends Vehicle {
-  constructor(make, model, year, numOfDoors) {
+class Car extends Vehicle {
+  constructor(make, model, year, numberOfDoors) {
     super(make, model, year);
-    this.numOfDoors = numOfDoors;
+    this.numberOfDoors = numberOfDoors;
   }
   getCarInfo() {
     return `${this.getVehicleInfo()}, Doors: ${this.numberOfDoors}`;
   }
 }
 class Bike extends Vehicle {
-    constructor(make, model, year, hasPedals) {
-      super(make, model, year); // Call the parent class constructor
-      this.hasPedals = hasPedals;
-    }
-  
-    getBikeInfo() {
-      return `${this.getVehicleInfo()}, Has Pedals: ${this.hasPedals ? 'Yes' : 'No'}`;
-    }
+  constructor(make, model, year, hasPedals) {
+    super(make, model, year);
+    this.hasPedals = hasPedals;
   }
+
+  getBikeInfo() {
+    return `${this.getVehicleInfo()}, Has Pedals: ${
+      this.hasPedals ? "Yes" : "No"
+    }`;
+  }
+}
+
+const myCar = new Car("Toyota", "Camry", 2021, 4);
+console.log(myCar.getCarInfo()); 
+
+const myBike = new Bike("Trek", "Domane", 2023, true);
+console.log(myBike.getBikeInfo()); 
