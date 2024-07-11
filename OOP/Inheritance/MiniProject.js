f79374819c62d4313a27cb36fc1a3666f039de5b
Zynalex9 <alzn952@gmail.com>
2024-07-11 21:08:54 +0500

    First commit

---
+++ OOP/Inheritance/MiniProject.js
@@ -0,0 +1,9 @@
+/*Base Class: Vehicle – This will be the parent class with common properties and methods.
+Derived Classes: Car and Bike – These will inherit from Vehicle and have additional specific properties and methods.*/
+class Vehicle{
+    constructor(make,model,year){
+        this.make= make
+        this.model= model
+        this.year= year
+    }
+}
